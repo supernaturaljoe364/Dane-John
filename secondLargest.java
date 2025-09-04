43aacef0d97afe4245d322ff8b5f66b95f086aa0
supernaturaljoe364 <supernaturaljoe364@gmail.com>
2025-09-04 22:25:47 +0530

    secondLargest element problem solved.

---
+++ secondLargest.java
@@ -0,0 +1,23 @@
+package learningJava;
+
+public class seondLargest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr = {8, 5, 2, 7, 3, 9, 1, 4};
+		int sLargest = 0;
+		int largest = 0;
+		for(int i : arr) {
+			if(i >= largest) {
+				sLargest = largest;
+				largest = i;
+			}
+			else if (i < largest && i >= sLargest) {
+				sLargest = i;
+			}
+		}
+		
+		System.out.println(sLargest);
+	}
+
+}
