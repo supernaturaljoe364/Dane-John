4b6de9b4bf663894de7770e7f2d4cc4caf756382
supernaturaljoe364 <supernaturaljoe364@gmail.com>
2025-09-05 13:25:36 +0530

    Sorting an array using the Dutch National Flag Algorithm

---
+++ Sort0s1s2s.java
@@ -0,0 +1,53 @@
+package learningJava;
+import java.util.Arrays;
+
+
+//USING THE DRY RUN/ DUTCH NATIONAL FLAG ALGORITHM
+public class Sort0s1s2s {
+
+    public static void main(String[] args) {
+        int[] arr = {0, 1, 1, 2, 1, 0, 2, 2, 0, 1, 0, 0};
+        int low = 0, mid = 0, high = arr.length - 1;
+
+        while (mid <= high) {
+            if (arr[mid] == 0) {
+                swap(arr, low, mid);
+                low++;
+                mid++;
+            } else if (arr[mid] == 1) {
+                mid++;
+            } else {
+                swap(arr, mid, high);
+                high--;
+            }
+        }
+
+        System.out.println(Arrays.toString(arr));
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
+
+
+//alternative approach using lists
+//	List<Integer> arr = new ArrayList<>(Arrays.asList(0, 1, 1, 2, 1, 0, 2, 2, 0, 1, 0, 0));
+//	int low = 0, mid = 0, high = arr.size()-1;
+//	while(mid <= high) {
+//		if(arr.get(mid) == 0) {
+//			Collections.swap(arr, low, mid);						//Collections.swap only work for Object arrays not prims
+//			low++; mid++;
+//		}
+//		else if (arr.get(mid) == 1) {
+//			mid++;
+//		}
+//		else {
+//			Collections.swap(arr	, mid, high);
+//			high--;
+//		}
+//	}
+//	
+//	System.out.println(arr);
