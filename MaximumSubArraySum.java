a9d74e7f7c46ef995ee779e25ad0eeb99304b630
chicane <84762835+supernaturaljoe364@users.noreply.github.com>
2025-09-22 18:54:09 +0530

    Update MaximumSubArraySum.java

--- MaximumSubArraySum.java
+++ MaximumSubArraySum.java
@@ -5,18 +5,14 @@ public class MaximumSubArraySum {
 	public static void main(String[] args) {
 		// using kadane's algorithm
 		int[] nums = {-2, -3, 4, -1, -2, 1, 5, -3};
-		int sum = 0;
-		int max = Integer.MIN_VALUE;
-		int n = nums.length;
-		for(int i = 0; i < n; i++) {
-			sum+= nums[i];
-			if (sum > max) {
-				max = sum;
-			}
-            else if (sum < 0) sum = 0;
-		}
+        int sum = 0;
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            max = Math.max(max, sum);   
+            if (sum < 0) sum = 0;     
+        }
 		System.out.println(max);
-
 	}
 
 }
